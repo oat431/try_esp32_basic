9f39de8cf25268b44a5eb92711fd7915e5fb518a
Oralita <54722120+oat431@users.noreply.github.com>
2022-09-28 00:17:23 +0700

    Add files via upload

--- hello_world_proj.ino
+++ hello_world_proj.ino
@@ -1,45 +1,41 @@
-//ADKeyboard Module
-//Developed by DFRobot.com
-//Last modified 30/11/2011
-//Version 1.0
 int adc_key_val[5] ={600,650, 700,  800, 900 };
 int NUM_KEYS = 5;
 int adc_key_in;
 int key=-1;
 int oldkey=-1;
-void setup()
-{
-  pinMode(13, OUTPUT);  //we'll use the debug LED to output a heartbeat
-  Serial.begin(9600); // 9600 bps
+int sw = 2;
+
+void setup(){
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop()
-{
+void loop(){
   adc_key_in = analogRead(0);    // read the value from the sensor
   digitalWrite(13,LOW);
   key = get_key(adc_key_in);  // convert into key press
-
-  if (key != oldkey)   // if keypress is detected
-   {
+  int val = digitalRead(sw);
+  if (val == 0) {
+    Serial.println("x");
+  }
+  if (key != oldkey){
     delay(50);  // wait for debounce time
     adc_key_in = analogRead(0);    // read the value from the sensor
     key = get_key(adc_key_in);    // convert into key press
-    if (key != oldkey)
-    {
+    if (key != oldkey){
       oldkey = key;
       if (key >=0){
         digitalWrite(13,HIGH);
-        switch(key)
-        {
-           case 0:Serial.println("S1 OK");
+        switch(key){
+           case 0:Serial.println("arrow up");
                   break;
-           case 1:Serial.println("S2 OK");
+           case 1:Serial.println("arrow left");
                   break;
-           case 2:Serial.println("S3 OK");
+           case 2:Serial.println("arrow down");
                   break;
-           case 3:Serial.println("S4 OK");
+           case 3:Serial.println("arrow right");
                   break;
-           case 4:Serial.println("S5 OK");
+           case 4:Serial.println("spacebar");
                   break;
         }
       }
@@ -47,17 +43,14 @@ void loop()
   }
  delay(100);
 }
-// Convert ADC value to key number
-int get_key(unsigned int input)
-{
+
+int get_key(unsigned int input){
     int k;
-    for (k = 0; k < NUM_KEYS; k++)
-    {
-      if (input < adc_key_val[k])
-     {
+    for (k = 0; k < NUM_KEYS; k++){
+      if (input < adc_key_val[k]){
             return k;
         }
    }
-       if (k >= NUM_KEYS)k = -1;  // No valid key pressed
-       return k;
+   if (k >= NUM_KEYS)k = -1;  // No valid key pressed
+   return k;
 }
